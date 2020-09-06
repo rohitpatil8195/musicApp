ac1d62b95f09464df4947c3be1762a011c26f180
rohitpatil8195 <rohitpatil8195@gmail.com>
2020-09-06 22:39:09 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,220 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, TouchableOpacity, View, Image, Text } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import { Audio } from 'expo-av';
+import { Asset } from 'expo-asset';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const audioBookPlaylist =[
+	{
+		title: 'song 1',
+		//author: 'Snoop Dogg',
+		source: 'Librivox',
+		uri:
+		Asset.fromModule(require('../Audio/assets/music/1.mp3')).uri
+	},
+	{
+		title: 'song 2',
+		//author: 'Snoop Dogg',
+		source: 'Librivox',
+		uri:
+		Asset.fromModule(require('../Audio/assets/music/2.mp3')).uri
+	},
+	{
+		title: 'song 3',
+		//author: 'Snoop Dogg',
+		source: 'Librivox',
+		uri:
+		Asset.fromModule(require('../Audio/assets/music/3.mp3')).uri
+	},
+	{
+				title: 'song 4',
+		author: 'Snoop Dogg',
+		source: 'Librivox',
+		uri:
+		Asset.fromModule(require('../Audio/assets/music/4.mp3')).uri
+	},
+	{
+		title: 'song 5',
+		//author: 'Snoop Dogg',
+		source: 'Librivox',
+		uri:
+		Asset.fromModule(require('../Audio/assets/music/5.mp3')).uri
+	},
+	{
+		title: 'song 6',
+		//author: 'Snoop Dogg',
+		source: 'Librivox',
+		uri:
+		Asset.fromModule(require('../Audio/assets/music/6.mp3')).uri
+	}
+]
+
+export default class App extends React.Component {
+	state = {
+		isPlaying: false,
+		playbackInstance: null,
+		currentIndex: 0,
+		volume: 1.0,
+		isBuffering: true
+	}
+
+	async componentDidMount() {
+		try {
+			await Audio.setAudioModeAsync({
+				allowsRecordingIOS: false,
+				interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+				playsInSilentModeIOS: true,
+				interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+				shouldDuckAndroid: true,
+				staysActiveInBackground: true,
+				playThroughEarpieceAndroid: true
+			})
+
+			this.loadAudio()
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
+	async loadAudio() {
+		const { currentIndex, isPlaying, volume } = this.state
+
+		try {
+			const playbackInstance = new Audio.Sound()
+			const source = {
+				uri: (audioBookPlaylist[currentIndex]).uri
+			}
+
+			const status = {
+				shouldPlay: isPlaying,
+				volume: volume
+			}
+
+			playbackInstance.setOnPlaybackStatusUpdate(this.onPlaybackStatusUpdate)
+			await playbackInstance.loadAsync(source, status, false)
+			this.setState({
+				playbackInstance
+			})
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
+	onPlaybackStatusUpdate = status => {
+		this.setState({
+			isBuffering: status.isBuffering
+		})
+	}
+
+	handlePlayPause = async () => {
+		const { isPlaying, playbackInstance } = this.state
+		isPlaying ? await playbackInstance.pauseAsync() : await playbackInstance.playAsync()
+
+		this.setState({
+			isPlaying: !isPlaying
+		})
+	}
+
+	handlePreviousTrack = async () => {
+		let { playbackInstance, currentIndex } = this.state
+		if (playbackInstance) {
+			await playbackInstance.unloadAsync()
+			currentIndex < audioBookPlaylist.length - 1 ? (currentIndex -= 1) : (currentIndex = 0)
+			this.setState({
+				currentIndex
+			})
+			this.loadAudio()
+		}
+	}
+
+	handleNextTrack = async () => {
+		let { playbackInstance, currentIndex } = this.state
+		if (playbackInstance) {
+			await playbackInstance.unloadAsync()
+			currentIndex < audioBookPlaylist.length - 1 ? (currentIndex += 1) : (currentIndex = 0)
+			this.setState({
+				currentIndex
+			})
+			this.loadAudio()
+		}
+	}
+
+	renderFileInfo() {
+		const { playbackInstance, currentIndex } = this.state
+		return playbackInstance ? (
+			<View style={styles.trackInfo}>
+				<Text style={[styles.trackInfoText, styles.largeText]}>
+					{audioBookPlaylist[currentIndex].title}
+				</Text>
+				<Text style={[styles.trackInfoText, styles.smallText]}>
+					{audioBookPlaylist[currentIndex].author}
+				</Text>
+				<Text style={[styles.trackInfoText, styles.smallText]}>
+					{audioBookPlaylist[currentIndex].source}
+				</Text>
+			</View>
+		) : null
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Image
+					style={styles.albumCover}
+					source={{ uri: 'https://st.depositphotos.com/2151617/4737/v/950/depositphotos_47370567-stock-illustration-music-background.jpg' }}
+				/>
+				<View style={styles.controls}>
+					<TouchableOpacity style={styles.control} onPress={this.handlePreviousTrack}>
+						<Ionicons name='ios-skip-backward' size={48} color='#444' />
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.control} onPress={this.handlePlayPause}>
+						{this.state.isPlaying ? (
+							<Ionicons name='ios-pause' size={48} color='#444' />
+						) : (
+							<Ionicons name='ios-play-circle' size={48} color='#444' />
+						)}
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.control} onPress={this.handleNextTrack}>
+						<Ionicons name='ios-skip-forward' size={48} color='#444' />
+					</TouchableOpacity>
+				</View>
+				{this.renderFileInfo()}
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	albumCover: {
+		width: 250,
+		height: 250
+	},
+	trackInfo: {
+		padding: 40,
+		backgroundColor: '#fff'
+	},
+
+	trackInfoText: {
+		textAlign: 'center',
+		flexWrap: 'wrap',
+		color: '#550088'
+	},
+	largeText: {
+		fontSize: 22
+	},
+	smallText: {
+		fontSize: 16
+	},
+	control: {
+		margin: 20
+	},
+	controls: {
+		flexDirection: 'row'
+	}
+})
